18ca38fb67c8d77abb98f92d2d5b6747500ce109
EnzoN7 <enzo1dimaria@gmail.com>
2022-03-30 20:40:20 +0200

    Update Main.ipynb

--- scripts/LoadData.py
+++ scripts/LoadData.py
@@ -5,36 +5,36 @@ import time
 import progressbar
 
 
-def load_data(data_path, classes, dataset='train', image_size=64):
+def load_data(_datapath: str, _classes: str, _dataset: str ='train', _imagesize: int =64):
     num_images = 0
-    for i in range(len(classes)):
-        dirs = sorted(os.listdir(data_path + dataset + '/' + classes[i]))
+    for i in range(len(_classes)):
+        dirs = sorted(os.listdir(_datapath + _dataset + '/' + _classes[i]))
         num_images += len(dirs)
 
-    x = np.zeros((num_images, image_size, image_size, 3))
+    x = np.zeros((num_images, _imagesize, _imagesize, 3))
     y = np.zeros((num_images, 1))
 
-    print("x_" + dataset + ".shape = " + str(x.shape) + "\ny_" + dataset + ".shape = " + str(y.shape))
+    print("x_" + _dataset + ".shape = " + str(x.shape) + "\ny_" + _dataset + ".shape = " + str(y.shape))
 
     current_index = 0
 
     # Parcours des différents répertoires pour collecter les images
     # Gestion de la progress bar : pip install progressbar2
     with progressbar.ProgressBar(max_value=10) as bar:
-        for idx_class in range(len(classes)):
-            dirs = sorted(os.listdir(data_path + dataset + '/' + classes[idx_class]))
+        for idx_class in range(len(_classes)):
+            dirs = sorted(os.listdir(_datapath + _dataset + '/' + _classes[idx_class]))
             num_images += len(dirs)
 
             # Chargement des images
             for idx_img in range(len(dirs)):
                 item = dirs[idx_img]
-                if os.path.isfile(data_path + dataset + '/' + classes[idx_class] + '/' + item):
+                if os.path.isfile(_datapath + _dataset + '/' + _classes[idx_class] + '/' + item):
                     # Ouverture de l'image
-                    img = Image.open(data_path + dataset + '/' + classes[idx_class] + '/' + item)
+                    img = Image.open(_datapath + _dataset + '/' + _classes[idx_class] + '/' + item)
                     # Conversion de l'image en RGB
                     img = img.convert('RGB')
                     # Redimensionnement de l'image et écriture dans la variable de retour x
-                    img = img.resize((image_size, image_size))
+                    img = img.resize((_imagesize, _imagesize))
                     x[current_index] = np.asarray(img)
                     # Écriture du label associé dans la variable de retour y
                     y[current_index] = idx_class
