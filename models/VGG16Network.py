b1d1a05c598242d37e6b72ba6bfe0651e69a9fe1
EnzoN7 <enzo1dimaria@gmail.com>
2022-04-02 15:38:19 +0200

    Validity of models

--- models/VGG16Network.py
+++ models/VGG16Network.py
@@ -11,12 +11,11 @@ class VGG16Network(tf.keras.Model):
                            include_top=False,
                            input_shape=(_imagesize, _imagesize, 3))
         self.flatten = Flatten()
-        self.dense1 = Dense(2048, activation='relu')
+        self.dense1 = Dense(256, activation='relu')
         self.dense2 = Dense(_nbclasses, activation='softmax')
 
     def call(self, inputs):
         x = self.vgg16(inputs)
         x = self.flatten(x)
-        x = self.dense1(x)
         x = self.dense1(x)
         return self.dense2(x)
