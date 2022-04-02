1243a5bb875b544b8b12954fc384bd2194e58666
EnzoN7 <enzo1dimaria@gmail.com>
2022-04-02 10:49:07 +0200

    Tests

--- scripts/Tests.py
+++ scripts/Tests.py
@@ -1,9 +1,10 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
-def test_data(_model, _labels, _idx, _lendata, _xtesti, _ytesti, _print):
+def test_data(_model, _labels, _idx, _xtest, _ytest, _print):
     data = []
-    data.append(_xtesti)
+    data.append(_xtest[_idx])
     data = np.asarray(data)
     dim = data[0].shape
     data = data.astype(np.float32).reshape(data.shape[0], dim[0], dim[1], dim[2])
@@ -14,11 +15,12 @@ def test_data(_model, _labels, _idx, _lendata, _xtesti, _ytesti, _print):
     res = prediction[0][idxBestPrediction] * 100
 
     if _print:
-        print("PREDICTIONS sur la donnée n°" + str(_idx) + "/" + str(_lendata - 1))
+        print("PREDICTIONS sur la donnée n°" + str(_idx) + "/" + str(len(_xtest) - 1))
         for i in range(0, len(_labels)):
             print('     ' + _labels[i] + ' -> ' + "{0:.2f}%".format(prediction[0][i] * 100.))
         print('\nRESULTAT :         ' + bestPrediction + ' / ' + "{0:.2f}%".format(res))
-        print('Résultat attendu : ' + _labels[int(_ytesti)])
+        print('Résultat attendu : ' + _labels[int(_ytest[_idx])])
+        plt.imshow(_xtest[_idx])
 
     return bestPrediction
 
@@ -26,7 +28,7 @@ def test_data(_model, _labels, _idx, _lendata, _xtesti, _ytesti, _print):
 def test_model(_model, _labels, _xtest, _ytest):
     sim = len(_xtest)
     for i in range(len(_xtest)):
-        bestPrediction = test_data(_model, _labels, 0, 0, _xtest[i], _ytest[i], False)
+        bestPrediction = test_data(_model, _labels, i, _xtest, _ytest, False)
         if (bestPrediction != _labels[int(_ytest[i])]):
             sim -= 1
     print("\nVALIDITE DU MODELE : " + "{0:.2f}%".format(sim / len(_xtest) * 100))
