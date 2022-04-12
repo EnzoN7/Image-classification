1d791f7024865f9ec7b9150f2e127144370937cd
EnzoN7 <enzo1dimaria@gmail.com>
2022-04-12 11:43:47 +0200

    Update Main.ipynb

--- scripts/Load.py
+++ scripts/Load.py
@@ -16,7 +16,8 @@ def load_data(_datapath: str, _classes, _dataset: str ='train', _imagesize: int 
     x = np.zeros((num_images, _imagesize, _imagesize, 3))
     y = np.zeros((num_images, 1))
 
-    print("x_" + _dataset + ".shape = " + str(x.shape) + "\ny_" + _dataset + ".shape = " + str(y.shape) + "\n")
+    print("DATASET         : " + _dataset)
+    print("NOMBRE D'IMAGES : " + str(num_images) + "\n")
 
     current_index = 0
 
